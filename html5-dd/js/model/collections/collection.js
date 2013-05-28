2ef02a0bfa6deea596f896c621377630e7737905
Om Shankar <invincible.om@gmail.com>
2013-05-28 16:35:22 +0530

    Update collection.js

--- html5-dd/js/model/collections/collection.js
+++ html5-dd/js/model/collections/collection.js
@@ -2,34 +2,34 @@ var HTML5DD = HTML5DD || {};
 
 (function(App) {
 
-	var ImagesList = Backbone.Collection.extend({
+    var ImagesList = Backbone.Collection.extend({
 
-		model: App.item,
+        model: App.item,
 
-		localStorage: new Store('HTML5DD'),
+        localStorage: new Store('HTML5DD'),
 
-		liked: function() {
-			return this.filter(function( item ) {
-				return item.get('liked');
-			});
-		},
+        liked: function() {
+            return this.filter(function( item ) {
+                return item.get('liked');
+            });
+        },
 
-		unliked: function() {
-			return this.without.apply( this, this.liked() );
-		},
+        unliked: function() {
+            return this.without.apply( this, this.liked() );
+        },
 
-		nextOrder: function() {
-			if ( !this.length ) {
-				return 1;
-			}
-			return this.last().get('order') + 1;
-		},
+        nextOrder: function() {
+            if ( !this.length ) {
+                return 1;
+            }
+            return this.last().get('order') + 1;
+        },
 
-		comparator: function( item ) {
-			return item.get('order');
-		}
-	});
+        comparator: function( item ) {
+            return item.get('order');
+        }
+    });
 
-	App.Collection = new ImagesList();
+    App.Collection = new ImagesList();
 
 }(HTML5DD));
